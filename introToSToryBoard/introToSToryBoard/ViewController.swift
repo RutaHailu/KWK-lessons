bbcdd18333fb6ec97aac706dc3deba9ab8821ba4
RutaHailu <110115961+RutaHailu@users.noreply.github.com>
2022-07-27 11:18:26 -0400

    This is our first x-code project

---
+++ introToSToryBoard/introToSToryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introToSToryBoard
+//
+//  Created by Scholar on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      print("This is a print statment-testing testing!")
+    }
+
+
+}
+
